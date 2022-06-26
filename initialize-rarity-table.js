e8f5c406b366e4716de8f27eaf070d78baf2bfaf
6luck <6luck6@gmail.com>
2022-06-26 19:18:05 +0000

    rarity table init script

---
+++ initialize-rarity-table.js
@@ -0,0 +1,20 @@
+const client = require('./connection.js')
+const parser = require('simple-excel-to-json')
+var doc = (parser.parseXls2Json('./parts-list.xlsx'))[0];
+
+let sql = 'INSERT INTO part_usage () VALUES'
+doc.forEach( (x,i) => {
+	sql += ' ()'
+	if(i!=doc.length){
+		sql += ','
+	}
+})
+
+client.connect( function(err){
+	if(err) throw err;
+	//client.query(sql);
+	doc.forEach((x) => {
+		if(x.PartType!=='')
+			client.query('INSERT INTO part_usage () VALUES ()');
+	})
+})
