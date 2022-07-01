0bc8d77ad38cac216f1825ab8dd479a010cd26cf
6luck <6luck6@gmail.com>
2022-07-01 16:49:17 +0000

    added metascore

--- rarity-for-potatoes.js
+++ rarity-for-potatoes.js
@@ -2,6 +2,7 @@ const client = require('./connection.js');
 let batch = 0;
 let batchSize = 10000;
 let p2p = [];
+let m2p = [];
 client.connect((e)=>{
 	if(e) throw e;
 	console.log("connected for applying rarity to potatoes")
@@ -9,7 +10,8 @@ client.connect((e)=>{
 	client.query("SELECT * FROM part_usage",function(err,res,fields){
 		//
 		res.forEach(function(part,i){
-			p2p[i+1] = part.metapoints
+			p2p[i+1] = part.rarity_factor
+			m2p[i+1] = part.metapoints
 			//console.log(part.metapoints)
 		})
 		updateNextBatch();
@@ -24,17 +26,21 @@ function updateNextBatch(){
 			return;
 		}
 		let topSQL = 'UPDATE potatoes SET rarity = CASE ID';
-		let SQL = '';
-		let bottomSQL = ' ELSE rarity END WHERE (ID>'+(batch*batchSize)+' AND ID<='+((batch+1)*batchSize)+')';
+		let pSQL = '';
+		let mSQL = '';
+		let _pSQL = ' ELSE rarity END, metascore = CASE ID';
+		let _mSQL = ' ELSE metascore END WHERE (ID>'+(batch*batchSize)+' AND ID<='+((batch+1)*batchSize)+')'
 		res.forEach((p)=>{
-			let score = p2p[p.nose] * p2p[p.mouth] * p2p[p.hat] /100 * p2p[p.eyes] * p2p[p.ears] * p2p[p.shoes]/100 * p2p[p.background] * p2p[p.leftarm] * p2p[p.rightarm];
+			let score = p2p[p.nose] * p2p[p.mouth] * p2p[p.hat] * p2p[p.eyes] * p2p[p.ears] * p2p[p.shoes] * p2p[p.background] * p2p[p.leftarm] * p2p[p.rightarm];
+			let metascore = m2p[p.nose] + m2p[p.mouth] + m2p[p.hat] + m2p[p.eyes] + m2p[p.ears] + m2p[p.shoes] + m2p[p.background] + m2p[p.leftarm] + m2p[p.rightarm]; 
 			//console.log("============\n==========\n=========\n",p.nose,p.mouth,p.hat,p.eyes,p.ears,p.shoes,p.background,p.leftarm,p.rightarm)
 			//console.log("Score "+p.ID+":::: ", score)
-			SQL += ' WHEN '+p.ID+' THEN '+score
+			pSQL += ' WHEN '+p.ID+' THEN '+score
+			mSQL += ' WHEN '+p.ID+' THEN '+metascore
 			
 		});
 		console.log("::::: Updating "+batch+':::::');
-		client.query( topSQL + SQL + bottomSQL , (err,res,fields)=>{
+		client.query( topSQL + pSQL + _pSQL + mSQL + _mSQL , (err,res,fields)=>{
 			if(err) throw err;
 			console.log("Batch "+batch+" updated");
 			batch+=1
