c466cf73a93a48498f8bbebec8ecd28252ac177b
6Luck <6luck6@gmail.com>
2022-07-02 04:13:33 -0400

    finished potato pool contract. it's simple. may parts moay come later but it should be pretty simple. Main reason we're not just sending it to any given address is because we want

--- potato-pool.sol
+++ potato-pool.sol
@@ -1,14 +1,13 @@
 pragma solidity ^0.8.14;
 contract PotatoPool{
-
+    address THIS = address(this);
     address contractOwner;
-    mapping(address => bool) worker; 
+    NFT POTATO = NFT(0x000000000000000);
+    mapping(address => bool) worker;
     
     constructor(){
         contractOwner = msg.sender;
     }
-
-    
 
     function changeContractOwner(address newContractOwner) public{
         require( msg.sender == contractOwner );
@@ -24,4 +23,21 @@ contract PotatoPool{
         require( msg.sender == contractOwner );
         worker[workerAddress] = false;
     }
+
+    event DockingPotato(address from, uint tokenId);
+    function onERC721Received(address from, uint256 tokenId, bytes calldata data) external returns (bytes4){
+        emit DockingPotato(from/* if it's from the machine, don't make an order ... if it's from someone else, make an order...*/, tokenId);
+    }
+
+    event SendPotato(address to, uint tokenId);
+    function transferNFT(address to, uint tokenId){
+        require( msg.sender     == contractOwner );
+        POTATO.transferFrom(THIS, address to, uint256 tokenId);
+        emit SendPotato(to, tokenId);
+    }
+}
+
+
+abstract contract NFT {
+    function transferFrom(address from, address to, uint256 tokenId) external;
 }
