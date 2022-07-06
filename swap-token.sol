273a0b1f4e7d9c199b6ad32823e4804729f905c0
6Luck <6luck6@gmail.com>
2022-07-06 01:45:28 -0400

    shake out phase 1

--- swap-token.sol
+++ swap-token.sol
@@ -1,13 +1,14 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.14;
 contract SwapToken{
     address THIS = address(this);
     address contractOwner;
-    ERC20 POTATO = NFT(0x000000000000000);
+    ERC20 POTATO = ERC20(0x0A72ffd37b8eb9cC72A9abF6B15d6Dac9d0BFA89);
     mapping(address => bool) worker;
     uint FEE;
     uint MAX_SWAP = 1000;
     uint collections;
-    
+
     constructor(){
         contractOwner = msg.sender;
     }
@@ -37,14 +38,14 @@ contract SwapToken{
         worker[workerAddress] = false;
     }
 
-    event DepositPotatoToken(address from, address for, uint amount);
-    function depositPotatoToken(address for, uint256 amount) external payable{
+    event DepositPotatoToken(address from, address forWhom, uint amount);
+    function depositPotatoToken(address forWhom, uint256 amount) external payable{
         address sender = msg.sender;
         uint cost = amount*FEE;
         require(msg.value == cost && amount<=MAX_SWAP);
         collections += cost;
         POTATO.transferFrom(sender, THIS, amount);
-        emit DepositPotatoToken(from, for, amount);
+        emit DepositPotatoToken(sender, forWhom, amount);
     }
     
     event SendPotato(address to, uint amount);
@@ -55,7 +56,7 @@ contract SwapToken{
 }
 
 
-abstract contract ERC20 {
+interface ERC20 {
     function transferFrom(address from, address to, uint256 amount) external;
-    function transfer(address from, address to, uint256 amount) external;
+    function transfer(address to, uint256 amount) external;
 }
